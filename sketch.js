be035c3bb526862737898108e293af7c7270c788
Mr hat <00001092919569sp@al.educacao.sp.gov.br>
2024-06-05 09:12:31 -0300

    adicionando arquivos js e ccs

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(400, 400);
+    background("black");
+  }
+  
+  function draw() {
+    stroke("blue");
+    fill("green");
+    
+    if (mouseIsPressed){
+      rect(mouseX, mouseY, 20, 35);
+    }
+  }
